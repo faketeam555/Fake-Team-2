810e9d6c8e5c27a3bb96f2cb9e72dc807d0037ff
rxnbyr <rxnbyr@gmail.com>
2018-07-28 15:52:37 +0530

    django-rq disabled new url added

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from analysis.views import check, report
+from analysis.views import check, report, bot_check
 
 urlpatterns = [
     path('check/', check, name='check'),
+    path('bot_check/', bot_check, name='bot_check'),
     path('report/', report, name='report'),
 ]
